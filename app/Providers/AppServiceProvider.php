7b42c812a7faad160864b5604ae68e802f8171d5
rasya_rhmt <151638383+RasyaRahmatSyaban@users.noreply.github.com>
2025-05-23 10:27:18 +0800

    update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env('APP_ENV') === 'production'){
-            URL::forceScheme('https')
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
         }
         Gate::define("view-student", function (User $user) {
             if($user->role === "admin" || $user->role === "guest") {
